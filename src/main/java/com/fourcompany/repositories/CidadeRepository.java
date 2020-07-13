ca71130e869813cb9b1c875341478f0625beb501
ValdeirEl <valdeirsantoselbano@gmail.com>
2020-07-13 13:01:08 -0400

    Estado e Cidade

---
+++ src/main/java/com/fourcompany/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package com.fourcompany.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fourcompany.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+	
+}
